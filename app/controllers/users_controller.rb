6d6102bc08669b9963d0f485badb68fff183b49c
Scott Goci <scottjg@gmail.com>
2012-06-07 17:55:09 -0400

    fixed leak for users trying to see other users information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+  	if current_user.id.to_s == params[:id]
+    	@user = User.find(params[:id])
+    else
+    	flash[:error] = 'Not authorized as an administrator.'
+    	redirect_to root_path
+    end
   end
 
 end
