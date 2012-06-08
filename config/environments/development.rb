38f335d4a52af79976b8e16835645d98159b689d
Scott Goci <scottjg@gmail.com>
2012-06-08 13:38:42 -0400

    working on getting email working for the app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,8 @@ Rails3BootstrapDeviseCancan::Application.configure do
     domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "testingleaks@gmail.com",
+    password: "1@@333$$$$"
   }
 
 
