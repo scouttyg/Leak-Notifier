b2a7c7674a695f991616cbfaedf98eb215cb8d62
Scott Goci <scottjg@gmail.com>
2012-06-08 12:56:08 -0400

    got storing of services for user, and let users edit it in their settings as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :services
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :services, :service_ids
   has_and_belongs_to_many :services, :join_table => "services_users"
+  before_destroy { |user| user.services.clear}
 end
