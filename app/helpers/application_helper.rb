a5052933c839b0fde9833c3335739d1bb5dfcefa
Scott Goci <sgoci@sazboom.com>
2012-06-11 11:14:53 -0400

    added google analytics for tracking hits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def google_analytics_js
+      content_tag(:script, :type => 'text/javascript') do
+              "var _gaq || [];
+        _gaq.push(['_setAccount', 'UA-15646891-15']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = document.getElementByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();"
+      end if Rails.env.production?
+    end
 end
