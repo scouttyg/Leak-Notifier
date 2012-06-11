aa5110f47c777e12d776fe81dc2411b9f2a85ca9
Scott Goci <sgoci@sazboom.com>
2012-06-10 23:00:35 -0400

    fixing precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails3BootstrapDeviseCancan::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += %w( select2.css select2.js select2.css.scss )
 
   # Generate digests for assets URLs
